d540338e7e834dea95128f5550b2e81392e1dcc1
blitz <146631108+blitzboah@users.noreply.github.com>
2025-02-08 21:44:53 +0530

    fuk py

--- GitRepository.java
+++ GitRepository.java
@@ -1,29 +1,62 @@
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class GitRepository {
-    private final String worktree = System.getProperty("user.dir");
-    private final File gitdir = new File(worktree + "/.gitz");
+    private final Path worktree;
+    private final Path gitdir;
 
-    void gitInit(){
-        if(gitdir.exists()) {
+    public GitRepository() {
+        this.worktree = Paths.get(System.getProperty("user.dir"));
+        this.gitdir = worktree.resolve(".gitz");
+    }
+
+    void gitInit() throws Exception {
+        if(gitdir.toFile().exists()) {
             System.out.println("already a gitz repo");
         }
         else {
-            if(gitdir.mkdir()){
+
+            if(gitdir.toFile().mkdir()) {
+
                 System.out.println("gitz repo initialized");
 
+                assert repoDir(new String[]{"branches"}, true) != null;
+                assert repoDir(new String[]{"objects"}, true) != null;
+                assert repoDir(new String[]{"refs", "tags"}, true) != null;
+                assert repoDir(new String[]{"refs", "heads"}, true) != null;
             }
+
             else {
                 System.out.println("failed to init gitz repo");
             }
         }
     }
 
-    void createFiles(){
-
+    Path repoPath(String... path){
+        return gitdir.resolve(Paths.get("", path));
     }
 
-    void createDirs(){
+    Path repoFile(String[] path, boolean mkdir) throws Exception {
+        if(repoDir(path, mkdir) == null)
+            return repoPath(path);
+        return null;
+    }
 
+    Path repoDir(String[] path, boolean mkdir) throws Exception {
+        Path resolevedPath = repoPath(path);
+
+        if(resolevedPath.toFile().exists()){
+            if(resolevedPath.toFile().isDirectory())
+                return resolevedPath;
+            else
+                throw new Exception("not a directory");
+        }
+
+        if (mkdir){
+            resolevedPath.toFile().mkdirs();
+            return resolevedPath;
+        }
+        else
+            return null;
     }
 }
